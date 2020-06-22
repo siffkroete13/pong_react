ee503a480ad931b66a267aa8dd2e32395e2bb4b1
Siffkroete <iwanp@gmx.ch>
2020-06-22 15:13:37 +0200

    bla

--- src/io/websocket_client.js
+++ src/io/websocket_client.js
@@ -11,16 +11,21 @@ class WebsocketClient extends Component {
     // eslint-disable-next-line no-useless-constructor
     constructor(props) {
         super(props);
-        this.init = this.init.bind(this);
+        this.initCallbacks = this.initCallbacks.bind(this);
         this.handleOpen = this.handleOpen.bind(this);
         this.handleMessage = this.handleMessage.bind(this);
         this.handleError = this.handleError.bind(this);
         this.handleClose = this.handleClose.bind(this);
 
-        this.init();
+        this.state = {
+            game_run: 'false',
+            name: ''
+        }
+
+        this.initCallbacks();
     }
     
-    init() {
+    initCallbacks() {
         client.onopen = this.handleOpen;
         client.onmessage = this.handleMessage;
         client.onerror = this.handleError;
@@ -43,9 +48,34 @@ class WebsocketClient extends Component {
         console.log('onclose ===> Verbindung wird geschlossen, ', msg);
     }
 
-    handleClick = (e) => {
-        console.log('handleClick ===> e: ', e);
+    handleInputChange = (e) => {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
+    }
 
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('handleClick ===> e: ', e);
+        this.setState({
+            game_run: true
+        });
+
+        const msg = {
+            msg_type: 'game_control',
+            payload: {
+                instruction: 'start_game',
+                name: this.state.name
+            }
+        }
+        
+        client.send(JSON.stringify(msg));
+    };
+
+    handleGameStateChange = (e) => {
         // ArrayBuffers are a good way to allocate space to work in. In the example below I allocate 32 Bytes.
         // I can then edit it by using one of Uint32Array, Uint16Array, Float32Array to write binary data
         // to different parts of the ArrayBuffer.
@@ -54,17 +84,20 @@ class WebsocketClient extends Component {
         // The websocket should recieve binary data as ArrayBuffers
         client.binaryType = 'arraybuffer';
         client.send(msg);
-
-        this.props.handleMessage(e); // Weiter nach oben leiten zur Parent-Component
-    };
+    }
     
     render() {
         return (
-            <div>
-                <button onClick={this.handleClick} >
-                    Click me
-                </button>
-            </div>
+            <>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name:
+                        <input type="text"
+                        onChange={this.handleInputChange} />
+                    </label>
+                    <input type="submit" value='Submit' />
+                </form>
+            </>
         );
     }
 }
